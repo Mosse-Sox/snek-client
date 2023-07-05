351049136529cc3548ba1491f50cd28df8994122
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-05 21:15:00 +0000

    adds a listener for input from the user and a function to handle that input, currently just ctrl + c to escape the program is functional.

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
-const net = require('net');
+// const net = require('net');
 const connect = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
 
 console.log('Connecting...');
 connect();
