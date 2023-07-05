befc7d75e8bb79204c5b05ac8a33e85d3678a9b8
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-05 21:28:32 +0000

    added a input.js file to contain all the code related to user inputs

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
 // const net = require('net');
 const connect = require('./client');
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
-
-const handleUserInput = (data) => {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
+const setupInput = require('./input');
 
 setupInput();
 
