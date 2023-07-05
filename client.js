2cf8882cda818edd8b2d3bb44c60e31730ae2fad
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-05 20:28:06 +0000

    added a client.js file to contain all code that is doing things with the connect

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log('You are now connected to the snek server');
+  });
+
+  conn.on('data', (data) => {
+    console.log(`Server: `, data);
+  });
+
+  
+
+  conn.on('close', () => {
+    console.log('Ended connection with the snek server');
+  });
+
+  return conn;
+};
+
+module.exports =  connect; 
--- play.js
+++ play.js
@@ -1,30 +1,6 @@
 const net = require('net');
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541,
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('connect', () => {
-    console.log('You are now connected to the snek server');
-  });
-
-  conn.on('data', (data) => {
-    console.log(`Server: `, data);
-  });
-
-  
-
-  conn.on('close', () => {
-    console.log('Ended connection with the snek server');
-  });
-
-  return conn;
-};
 
 console.log('Connecting...');
 connect();
